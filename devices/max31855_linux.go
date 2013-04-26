18d857d92e45d1cdeae3236a247364d374d2a75e
Bobby Powers <bobbypowers@gmail.com>
2013-04-26 01:26:38 -0400

    max31855: read Celsius data from thermocouple

--- devices/max31855_linux.go
+++ devices/max31855_linux.go
@@ -32,9 +32,7 @@ func (m *Max31855) Read() (Celsius, error) {
 		return 0, fmt.Errorf("spi.Transaction(%v, nil, buf): %s", m.f, err)
 	}
 
-	fmt.Printf("omfg, read something: %#v\n", buf)
-
-	return 0, fmt.Errorf("not implemented")
+	return Celsius(.25 * float64(buf[0] << 6 | buf[1] >> 2)), nil
 }
 
 func (m *Max31855) Close() {
